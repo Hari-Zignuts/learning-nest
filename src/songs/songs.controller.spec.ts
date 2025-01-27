90407660db6732b1caff9f5e67b48f68a3a69cee
Hari Malam <harim@zignuts.com>
2025-01-27 16:52:14 +0530

    fix: songs controller spec

--- src/songs/songs.controller.spec.ts
+++ src/songs/songs.controller.spec.ts
@@ -1,18 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SongsController } from './songs.controller';
+import { SongsService } from './songs.service';
 
 describe('SongsController', () => {
   let controller: SongsController;
+  let service: SongsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SongsController],
+      providers: [SongsService],
     }).compile();
 
     controller = module.get<SongsController>(SongsController);
+    service = module.get<SongsService>(SongsService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+    expect(service).toBeDefined();
   });
 });
