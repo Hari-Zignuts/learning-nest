d495f9cb399f4b30f7b1967b6d40aa3c9186ee31
Hari Malam <harim@zignuts.com>
2025-01-28 15:46:24 +0530

    fix: songs controller & service spec

--- src/songs/songs.controller.spec.ts
+++ src/songs/songs.controller.spec.ts
@@ -1,23 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SongsController } from './songs.controller';
 import { SongsService } from './songs.service';
+import { SongRepository } from './repositories/song.repository';
 
 describe('SongsController', () => {
   let controller: SongsController;
   let service: SongsService;
+  let songRepository: SongRepository;
 
   beforeEach(async () => {
+    const mockSongRepository = {
+      find: jest.fn(),
+      findOne: jest.fn(),
+      create: jest.fn(),
+      save: jest.fn(),
+      remove: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SongsController],
-      providers: [SongsService],
+      providers: [
+        SongsService,
+        {
+          provide: SongRepository,
+          useValue: mockSongRepository,
+        },
+      ],
     }).compile();
 
     controller = module.get<SongsController>(SongsController);
     service = module.get<SongsService>(SongsService);
+    songRepository = module.get<SongRepository>(SongRepository);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
     expect(service).toBeDefined();
+    expect(songRepository).toBeDefined();
   });
 });
--- src/songs/songs.service.spec.ts
+++ src/songs/songs.service.spec.ts
@@ -1,18 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SongsService } from './songs.service';
+import { SongRepository } from './repositories/song.repository';
 
 describe('SongsService', () => {
   let service: SongsService;
+  let repository: SongRepository;
 
   beforeEach(async () => {
+    const mockSongRepository = {
+      find: jest.fn(),
+      findOne: jest.fn(),
+      create: jest.fn(),
+      save: jest.fn(),
+      remove: jest.fn(),
+    };
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SongsService],
+      providers: [
+        SongsService,
+        {
+          provide: SongRepository,
+          useValue: mockSongRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<SongsService>(SongsService);
+    repository = module.get<SongRepository>(SongRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+    expect(repository).toBeDefined();
   });
 });
