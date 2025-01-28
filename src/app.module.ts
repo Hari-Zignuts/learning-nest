c26b7c000bd601cd57a37e4f64cd171e64736b0b
Hari Malam <harim@zignuts.com>
2025-01-28 11:23:55 +0530

    feat: establish PostgreSQL database connection using TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,41 @@ import {
   NestModule,
   RequestMethod,
 } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { DataSource } from 'typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
+import { AppController } from './app.controller';
 import { SongsModule } from './songs/songs.module';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
+
 // import { SongsController } from './songs/songs.controller';
 
 @Module({
-  imports: [SongsModule],
+  imports: [
+    SongsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'postgres',
+      password: 'ztlab138',
+      database: 'spotify_clone',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule implements NestModule {
+  // check the database connection
+  constructor(readonly dataSource: DataSource) {
+    console.log(
+      `Database connection to ${dataSource.driver.database}: ${dataSource.isConnected}`,
+    );
+  }
+
+  // Middleware configuration
   configure(consumer: MiddlewareConsumer) {
     // Option - 1 // Apply middleware to a specific route in the songs module
     consumer.apply(LoggerMiddleware).forRoutes({
