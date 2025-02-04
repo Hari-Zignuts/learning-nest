1ed91d89f4f1446be56ed37a37c521fb5c314fed
Hari Malam <harim@zignuts.com>
2025-02-04 12:27:31 +0530

    env validation

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import configuration from './configuration';
+import { envValidationSchema } from './env.validation';
 
 @Module({
   imports: [
@@ -8,6 +9,11 @@ import configuration from './configuration';
       load: [configuration],
       envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
       isGlobal: true,
+      validationSchema: envValidationSchema,
+      validationOptions: {
+        allowUnknown: true,
+        abortEarly: true,
+      },
     }),
   ],
 })
